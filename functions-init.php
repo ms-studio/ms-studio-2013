6f83624bb01f5e6d2ea57567f34dc3a39f8d39f8
Manuel Schmalstieg <git.code@ms-studio.net>
2013-10-29 16:52:34 +0100

    update css version

--- functions-init.php
+++ functions-init.php
@@ -7,7 +7,7 @@ function custom_register_styles() {
 				// the MAIN stylesheet
 				wp_enqueue_style( 
 						'main_css_style', 
-						get_stylesheet_directory_uri() . '/css/ms-dva.min-2013-09-28c.css', // main.css
+						get_stylesheet_directory_uri() . '/css/ms-dva.min-2013-10-24.css', // main.css
 						false, // dependencies
 						null // version
 				); 
