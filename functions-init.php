63a75c8fd017dbe03d75f8ad67d750491756e7c3
Manuel Schmalstieg <git.code@ms-studio.net>
2013-12-12 02:50:53 +0100

    make that WP stats smiley invisible...

--- functions-init.php
+++ functions-init.php
@@ -7,7 +7,7 @@ function custom_register_styles() {
 				// the MAIN stylesheet
 				wp_enqueue_style( 
 						'main_css_style', 
-						get_stylesheet_directory_uri() . '/css/ms-dva.min-2013-12-09y.css', // main.css
+						get_stylesheet_directory_uri() . '/css/ms-dva.min-2013-12-11.css', // main.css
 						false, // dependencies
 						null // version
 				); 
